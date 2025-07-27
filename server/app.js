040b3e1ef21744f1db54589fc4ded3d7d79ab530
unknown <prashantpattanashetti5@gmail.com>
2025-07-27 21:01:05 +0530

    authentication feature added

--- server/app.js
+++ server/app.js
@@ -4,24 +4,23 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from "dotenv";
 import storyRoutes from "./routes/stories.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json({limit : "32mb", extended:true}));
-app.use(bodyParser.urlencoded  ({limit : "32mb", extended:true}));
+app.use(bodyParser.urlencoded({limit : "32mb", extended:true}));
 app.use(cors());
-
 app.use("/stories",storyRoutes);
-
+app.use("/user",userRoutes);
 const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5001;
 
 const connectDB = async () =>{
-
     try{
        await mongoose.connect(MONGO_URI);
-       app.listen(PORT, () => console.log(`Server Running on port:  ${PORT}`));
+       app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`));
     }catch(err){
         console.error("Connection to MongoDB failed", err.message);
     }
@@ -30,5 +29,4 @@ const connectDB = async () =>{
 connectDB();
 
 mongoose.connection.on("open", () =>console.log("Connection to database has been established successfully"));
-
-mongoose.connection.on("error", (err) =>console.log("err")); 
+mongoose.connection.on("error", (err) =>console.log(err)); 
---
+++ server/controllers/users.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+import bcyrpyt from 'bcryptjs';
+
+import User from "../models/user.js";
+
+const login = async(req, res){
+
+};
+
+const signup = async(req, res){
+
+};
+
